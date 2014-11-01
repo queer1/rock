032a73aa3dae64b2f7c8d8af450c536232d4d32d
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-11-01 20:50:29 +0100

    Add instrument type to Score.
    Fixes #3

--- grails-app/domain/de/dewarim/rock/Score.groovy
+++ grails-app/domain/de/dewarim/rock/Score.groovy
@@ -14,8 +14,39 @@ class Score {
     Integer longestStreak = 0
     Integer precisionTimesTen = 0
     Integer abilityTimesTen = 0
-    ScoreType type
+    ScoreType scoreType
+    InstrumentType instrumentType
 
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (!(o instanceof Score)) return false
 
+        Score score = (Score) o
 
+        if (abilityTimesTen != score.abilityTimesTen) return false
+        if (dateCreated != score.dateCreated) return false
+        if (id != score.id) return false
+        if (longestStreak != score.longestStreak) return false
+        if (person != score.person) return false
+        if (points != score.points) return false
+        if (precisionTimesTen != score.precisionTimesTen) return false
+        if (ranking != score.ranking) return false
+        if (song != score.song) return false
+        if (scoreType != score.scoreType) return false
+        if (instrumentType != score.instrumentType) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = (points != null ? points.hashCode() : 0)
+        result = 31 * result + (ranking != null ? ranking.hashCode() : 0)
+        result = 31 * result + dateCreated.hashCode()
+        result = 31 * result + (longestStreak != null ? longestStreak.hashCode() : 0)
+        result = 31 * result + (precisionTimesTen != null ? precisionTimesTen.hashCode() : 0)
+        result = 31 * result + (abilityTimesTen != null ? abilityTimesTen.hashCode() : 0)
+        result = 31 * result + scoreType.hashCode()
+        return result
+    }
 }
---
+++ src/java/de/dewarim/rock/InstrumentType.java
@@ -0,0 +1,11 @@
+package de.dewarim.rock;
+
+/**
+ */
+public enum InstrumentType {
+
+    LEAD_GUITAR,
+    RHYTHM_GUITAR,
+    BASS
+
+}
